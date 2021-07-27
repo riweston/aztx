b87d2ee29cf5785a39662dc09738439a648b8368
Richard Weston <gpg@riweston.io>
2021-07-27 08:42:19 +0100

    feat: Add some dumb tests to check for binaries

---
+++ test/aztx_test.go
@@ -0,0 +1,22 @@
+package aztx_test
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestAzBinary(t *testing.T) {
+	_, err := exec.LookPath("az")
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+func TestFzfBinary(t *testing.T) {
+	_, err := exec.LookPath("fzf")
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
