5215131114164d42f9a4fc4af7f7fd4ab9158f6b
Richard Weston <gpg@riweston.io>
2021-08-01 12:09:38 +0100

    test: Update type on the helper function

--- test/helpers_test.go
+++ test/helpers_test.go
@@ -27,7 +27,7 @@ func ok(tb testing.TB, err error) {
 }
 
 // equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
+func equals(tb testing.TB, exp interface{}, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
