23596d0ad25ce30c4240a81bb259d2f0a31999bd
Richard Weston <github@riweston.io>
2024-03-21 09:07:39 +0000

    fix: Stripped these fields out as they're not used and can cause issues with the json unmarshal

--- pkg/profile/models.go
+++ pkg/profile/models.go
@@ -15,11 +15,15 @@ type Subscription struct {
 		Name string `json:"name"`
 		Type string `json:"type"`
 	} `json:"user"`
-	IsDefault        bool      `json:"isDefault"`
-	TenantID         uuid.UUID `json:"tenantId"`
-	EnvironmentName  string    `json:"environmentName"`
-	HomeTenantID     uuid.UUID `json:"homeTenantId"`
-	ManagedByTenants []struct {
-		TenantID uuid.UUID `json:"tenantId"`
-	} `json:"managedByTenants"`
+	IsDefault bool `json:"isDefault"`
+
+	// These fields are commented out in case they are needed in the future.
+	// They cause some issues with unmarshalling if they're not present in the JSON, as they're not used we'll just ignore them
+
+	//TenantID         uuid.UUID `json:"tenantId"`
+	//EnvironmentName  string    `json:"environmentName"`
+	//HomeTenantID     uuid.UUID `json:"homeTenantId"`
+	//ManagedByTenants []*struct {
+	//	TenantID uuid.UUID `json:"tenantId"`
+	//} `json:"managedByTenants"`
 }
